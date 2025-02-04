c1f2ea41038a6c5e3807210ff239718df4ca7ae6
natassia-nn <natassianguyen@gmail.com>
2025-02-04 10:17:21 -0800

    complete

--- Main.java
+++ Main.java
@@ -37,13 +37,42 @@ public class Main
 
   public static boolean hasDuplicates(int[] arr)
   {
-    // replace with your code
+    for (int i=0; i < arr.length; i++)
+    {
+      int target = arr[i];
+      
+      for (int k= i+1; k < arr.length; k++)
+      {
+        if (arr[k].equals(target))
+        {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
   public static String findMode(String[] arr)
   {
-    // replace with your code
-    return null;
+    int count = 0;
+    int mode = arr[0];
+    int max_count = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+      String target = arr[i];
+      count = 0;
+      for (int j = 0; j < arr.length; j++)
+      if (arr[j] == target)
+      {
+        count++;
+      }
+    }
+
+    if (count > max_count)
+    {
+      max_count = count;
+      mode = arr[i];
+    }
+    return mode;
   }
 }
